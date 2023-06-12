94e93e2080e3baaf53683b9d347c7cb19ceb34ad
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-06-12 19:05:50 -0400

    Add msg of successful connection, name sending

--- client.js
+++ client.js
@@ -5,11 +5,20 @@ const connect = function () {
     host: 'localhost',
     port: 50541,
   });
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  });
+
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
   // handle incoming data and console.log it for the player
   conn.on("data", (data) => {
     console.log("Server says: ", data);
+  });
+// send string to the server
+  conn.on("connect", () => {
+    conn.write("Name: MT");
   });
   return conn;
 };
