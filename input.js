506e4a192a124163993cecc6eeb82e1e5cef79ca
Maria Tiagunova <tiagunova.maria@gmail.com>
2023-06-14 14:34:44 -0400

    add movement keys to constants

--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+const { MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY } = require("./constants");
+
 // setup interface to handle user input from stdin
 let connection;
 const setupInput = function(conn) {
@@ -16,17 +18,16 @@ const handleUserInput = function(key) {
     process.exit();
   }
   // WASD movement
-  if (key === 'w') {
-  // console.log("Move: up");
+  if (key === MOVE_UP_KEY) {
     connection.write("Move: up");
   }
-  if (key === 'a') {
+  if (key === MOVE_LEFT_KEY) {
     connection.write("Move: left");
   }
-  if (key === 's') {
+  if (key === MOVE_DOWN_KEY) {
     connection.write("Move: down");
   }
-  if (key === 'd') {
+  if (key === MOVE_RIGHT_KEY) {
     connection.write("Move: right");
   }
   if (key === 'g') {
